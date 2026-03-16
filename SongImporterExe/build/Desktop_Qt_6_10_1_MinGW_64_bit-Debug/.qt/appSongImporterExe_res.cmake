dbb98eb1dd1c448485be6b480f5cab33bae7cd54
gaslam <44768183+gaslam@users.noreply.github.com>
2026-03-16 18:15:44 +0100

    Remove build

--- SongImporterExe/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appSongImporterExe_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appSongImporterExe")
-set(working_dir "E:/SongImporter/SongImporterExe")
-set(src_and_dest_list
-
-)
-set(timestamp_file "E:/SongImporter/SongImporterExe/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appSongImporterExe_res.txt")
