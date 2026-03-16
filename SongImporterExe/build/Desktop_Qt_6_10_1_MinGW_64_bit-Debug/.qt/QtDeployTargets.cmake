4ea1a05088cc0ccbcd1618bb0a515ca1469da94e
gaslam <44768183+gaslam@users.noreply.github.com>
2026-03-16 18:14:49 +0100

    Move .gitignore

---
+++ SongImporterExe/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_appSongImporterExe_FILE E:/SongImporter/SongImporterExe/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/appSongImporterExe.exe)
+set(__QT_DEPLOY_TARGET_appSongImporterExe_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_appSongImporterExe_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6QmlMeta.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6QmlWorkerScript.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
---
+++ SongImporterExe/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appSongImporterExe_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appSongImporterExe")
+set(working_dir "E:/SongImporter/SongImporterExe")
+set(src_and_dest_list
+    "E:/SongImporter/SongImporterExe/Main.qml"
+    "E:/SongImporter/SongImporterExe/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/SongImporterExe/Main.qml"
+
+)
+set(timestamp_file "E:/SongImporter/SongImporterExe/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appSongImporterExe_qml.txt")
