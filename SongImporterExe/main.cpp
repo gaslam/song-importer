46fc6aad0f5971a9af26448e2b55a4eeeb2ab1e1
gaslam <gaspard.lammertyn@student.howest.be>
2026-03-18 18:44:40 +0100

    Register file validator

--- SongImporterExe/main.cpp
+++ SongImporterExe/main.cpp
@@ -3,12 +3,14 @@
 #include <SongImporterLib_global.h>
 #include <InputValidator.h>
 #include <FolderValidator.h>
+#include <FileValidator.h>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
     qmlRegisterType<FolderValidator>("SongImporter.Validators", 1, 0, "FolderValidator");
     qRegisterMetaType<ValidationResult>("validationResult");
+    qmlRegisterType<FileValidator>("SongImporter.Validators", 1, 0, "FileValidator");
     QQmlApplicationEngine engine;
     QObject::connect(
         &engine,
