216db09ed9412419b4d92ed39d823ac321fc18e4
gaslam <gaspard.lammertyn@student.howest.be>
2026-03-20 18:26:14 +0100

    Add supported software install path

--- SongImporterLib/FileUtils.h
+++ SongImporterLib/FileUtils.h
@@ -1,22 +1,48 @@
 #ifndef FILEUTILS_H
 #define FILEUTILS_H
 
-#include <QObject>
 #include "SongImporterLib_global.h"
+
+#include <QObject>
+#include <QStandardPaths>
 #include <QUrl>
+#include <QDir>
 
 
 class SONGIMPORTERLIB_EXPORT FileUtils : public QObject {
     Q_OBJECT
 
+    // Enum for supported software
+    enum SupportedSoftware {
+        Rekordbox,
+        // add more as needed
+    };
+    Q_ENUM(SupportedSoftware)
+
 public:
     explicit FileUtils(QObject* parent = nullptr) : QObject{parent}
         {
-
         }
 public slots:
     [[nodiscard]] inline static QString localPath(const QUrl &input) {
         return input.toLocalFile();
+    }
+
+    [[nodiscard]] inline static QString softwareDataLocation(const SupportedSoftware& software)
+    {
+        QDir installDir{QStandardPaths::writableLocation(QStandardPaths::AppDataLocation)};
+
+        installDir = installDir.path();
+
+        switch(software)
+        {
+
+        case Rekordbox:
+            installDir = installDir.filePath("rekordbox/rekordbox/rekordbox.xml");
+            break;
+        }
+
+        return installDir.absolutePath();
     }
 };
 
