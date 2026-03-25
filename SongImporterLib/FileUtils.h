2766162e44ab5f801c61b75f4ecce5cb429ef3f0
gaslam <gaspard.lammertyn@student.howest.be>
2026-03-25 17:02:28 +0100

    Add IsZipFile function to utils

--- SongImporterLib/FileUtils.h
+++ SongImporterLib/FileUtils.h
@@ -7,6 +7,7 @@
 #include <QStandardPaths>
 #include <QUrl>
 #include <QDir>
+#include "OperationResult.h"
 
 
 class SONGIMPORTERLIB_EXPORT FileUtils : public QObject {
@@ -21,8 +22,8 @@ class SONGIMPORTERLIB_EXPORT FileUtils : public QObject {
 
 public:
     explicit FileUtils(QObject* parent = nullptr) : QObject{parent}
-        {
-        }
+    {
+    }
 public slots:
     [[nodiscard]] inline static QString localPath(const QUrl &input) {
         return input.toLocalFile();
@@ -48,6 +49,43 @@ public slots:
         }
 
         return installDir.absolutePath();
+    }
+
+    [[nodiscard]] inline static OperationResult isZipFile(const QUrl &url)
+    {
+        QFile f(url.toLocalFile());
+        OperationResult result{};
+        if (!f.open(QIODevice::ReadOnly))
+        {
+
+            result.isSuccessful = false;
+            result.error = f.fileName() + "is read-only. Set the permisions to read/write to continue.";
+            return result;
+        }
+
+        const QByteArray sig = f.read(4);
+        if (sig == "PK\x03\x04") // Normal ZIP
+        {
+            result.isSuccessful = true;
+            return result;
+        }
+        else if (sig == "PK\x05\x06") // empty ZIP
+        {
+            result.error = "ZIP archive is empty.";
+            result.isSuccessful = false;
+            return result;
+        }
+        else if (sig == "PK\x07\x08") //Spanned ZIP
+        {
+            result.error = "ZIP archive is spanned (multi-part), which is not supported.";
+            result.isSuccessful = false;
+            return result;
+        }
+
+        result.error = "File is not a valid ZIP archive.";
+        result.isSuccessful = false;
+        return result;
+
     }
 };
 
