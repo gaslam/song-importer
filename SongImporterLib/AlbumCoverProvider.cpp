f09ccb85d56493abfb316bfaad3fd0691393fa7e
gaslam <gaspard.lammertyn@student.howest.be>
2026-03-27 19:49:03 +0100

    Add albumcover provider

---
+++ SongImporterLib/AlbumCoverProvider.cpp
@@ -0,0 +1,41 @@
+#include "AlbumCoverProvider.h"
+
+AlbumCoverProvider::AlbumCoverProvider(const QString& defaultId, const QImage& defaultImage)
+    : QQuickImageProvider(QQuickImageProvider::Image),
+    m_DefaultId{defaultId}
+{
+    addImage(defaultId,defaultImage);
+}
+
+void AlbumCoverProvider::addImage(const QString &id, const QImage &image)
+{
+    if(!m_Images.contains(id))
+    {
+        m_Images[id] = image;
+    }
+}
+
+QImage AlbumCoverProvider::requestImage(const QString &id, QSize *size, const QSize &requestedSize)
+{
+    QImage image;
+    if(m_Images.contains(id))
+    {
+        image = m_Images[id];
+
+    }
+    else if(m_Images.contains(m_DefaultId))
+    {
+        image = m_Images[m_DefaultId];
+    }
+
+    if(!image.isNull())
+    {
+        if (size)
+            *size = image.size();
+
+        if (requestedSize.width() > 0 && requestedSize.height() > 0)
+            image = image.scaled(requestedSize, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    }
+
+    return image;
+}
---
+++ SongImporterLib/AlbumCoverProvider.h
@@ -0,0 +1,25 @@
+#ifndef ALBUMCOVERPROVIDER_H
+#define ALBUMCOVERPROVIDER_H
+
+#include "SongImporterLib_global.h"
+#include <QQuickImageProvider>
+
+class SONGIMPORTERLIB_EXPORT AlbumCoverProvider : public QQuickImageProvider
+{
+public:
+    AlbumCoverProvider(const QString& defaultId, const QImage& defaultImage);
+
+
+public:
+    void addImage(const QString &id, const QImage& image);
+
+private:
+    const QString m_DefaultId;
+    QHash<QString,QImage> m_Images;
+
+    // QQuickImageProvider interface
+public:
+    QImage requestImage(const QString &id, QSize *size, const QSize &requestedSize) override;
+};
+
+#endif // ALBUMCOVERPROVIDER_H
